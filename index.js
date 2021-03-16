9337ced2117d8c8c075025a0735f971a8d2477b1
Muhamad Gameel <mg0x16@gmail.com>
2021-03-17 01:21:03 +0200

    add and link react native navigation module

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-/**
- * @format
- */
+import {Navigation} from 'react-native-navigation';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import HomeScreen from './src/pages/Home';
 
-AppRegistry.registerComponent(appName, () => App);
+Navigation.registerComponent('com.movieappcc.HomeScreen', () => HomeScreen);
+
+Navigation.events().registerAppLaunchedListener(() => {
+  Navigation.setRoot({
+    root: {
+      stack: {
+        children: [
+          {
+            component: {
+              name: 'com.movieappcc.HomeScreen',
+            },
+          },
+        ],
+      },
+    },
+  });
+});
